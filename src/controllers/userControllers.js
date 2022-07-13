6352aca888f056ab4189916defca6541f78cbda7
nmontiel <58341731+nemontiel@users.noreply.github.com>
2022-07-13 16:26:41 -0400

    add jwt and cookie

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,24 +1,33 @@
 const bcrypt = require("bcryptjs");
-const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
 const { User } = require("../models");
 
 const userControllers = {
   signUp: async (req, res) => {
     const { email, password } = req.body;
-    const hashedPassword = bcrypt.hashSync(password);
-    const user = await User.create({
-      email,
-      password: hashedPassword,
-    });
-    res.json({
-      success: true,
-      res: {
-        email: email,
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const user = await User.create(
+      {
+        email,
         password: hashedPassword,
       },
-    });
+      { fields: ["email", "password"] }
+    );
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY
+    );
+
+    // Guarda jwt en cookie
+    req.session.jwt = userJwt;
+    res.status(201).send(user);
   },
+  signIn: async (req, res) => {},
 };
 
 module.exports = userControllers;
