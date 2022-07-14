7430fc9798119272f701043cdb06833db680c365
nmontiel <58341731+nemontiel@users.noreply.github.com>
2022-07-13 21:33:22 -0400

    add sign in and sign out

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -25,9 +25,58 @@ const userControllers = {
 
     // Guarda jwt en cookie
     req.session.jwt = userJwt;
-    res.status(201).send(user);
+    res
+      .status(201)
+      .json({ succes: true, res: { email: user.email, id: user.id } });
   },
-  signIn: async (req, res) => {},
+  signIn: async (req, res) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({
+      where: { email: email },
+    });
+
+    if (!existingUser) {
+      res.status(401).json({ succes: false, message: "User was not founded" });
+    }
+    const passwordMatch = await bcrypt.compareSync(
+      password,
+      existingUser.password
+    );
+
+    if (passwordMatch) {
+      const userJwt = jwt.sign(
+        {
+          id: existingUser.id,
+          email: existingUser.email,
+        },
+        process.env.JWT_KEY
+      );
+
+      // Guarda jwt en cookie
+      req.session.jwt = userJwt;
+      res.status(200).json({
+        succes: true,
+        res: { email: existingUser.email, id: existingUser.id },
+      });
+    } else {
+      res.json({ succes: false, message: "Wrong password provided." });
+    }
+  },
+  currentUser: async (req, res) => {
+    if (!req.session?.jwt) {
+      return res.send({ currentUser: null });
+    }
+    try {
+      const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY);
+      res.send({ currentUser: payload });
+    } catch (err) {
+      res.send({ currentUser: null });
+    }
+  },
+  signOut: async (req, res) => {
+    req.session = null;
+    res.send({});
+  },
 };
 
 module.exports = userControllers;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const userControllers = require("../controllers/userControllers");
 const validator = require("../controllers/validator");
 
-const { signUp, signIn } = userControllers;
+const { signUp, signIn, currentUser, signOut } = userControllers;
 
 router.route("/users/signup").post(validator, signUp);
-router.route("/users/signin").post(signIn);
+router.route("/users/signin").post(validator, signIn);
+router.route("/users/signout").post(signOut);
+router.route("/users/currentuser").get(currentUser);
 
 module.exports = router;
