d4602e23e050197eb2d032321f4994ce044433d7
nmontiel <58341731+nemontiel@users.noreply.github.com>
2022-07-15 16:58:59 -0400

    add swagger for api  documentation

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const cookieSession = require("cookie-session");
+const path = require("path");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc");
 
 const { sequelize } = require("./src/models/index");
 
 const userRoutes = require("./src/routes/userRoutes");
 
 const app = express();
+
+const swaggerSpec = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "dlab-c3 API",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  apis: [`${path.join(__dirname, "./src/routes/*.js")}`],
+};
 
 async function connectDb() {
   try {
@@ -27,6 +46,11 @@ app.use(
 );
 
 app.use("/api/1", userRoutes);
+app.use(
+  "/api/1/documentation",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerJSDoc(swaggerSpec))
+);
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`App running on port: ${process.env.APP_PORT}`);
