f5f924f2707d3a4fbb569ac6b4519d13fa033f0a
nmontiel <58341731+nemontiel@users.noreply.github.com>
2022-10-24 17:08:31 -0300

    get tests

--- src/controllers/testController.js
+++ src/controllers/testController.js
@@ -13,7 +13,11 @@ const testController = {
       ],
     });
     if (userTests.length !== 0) {
+<<<<<<< HEAD
       res.status(200).json({ succes: true, res: userTests });
+=======
+      res.status(200).json({ succes: true, res: userShows });
+>>>>>>> 785b9ff (tests)
     } else {
       res.status(204).json({});
     }
